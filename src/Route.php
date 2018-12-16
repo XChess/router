7e5b5ee79ac78296e6299c6951e50bd863fb06b0
Nicholas <45908801+iszorpal@users.noreply.github.com>
2018-12-16 01:13:47 -0500

    Add route class.

--- src/Route.php
+++ src/Route.php
@@ -1,1 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * Router - Crystal Framework.
+ * The easiest to use php router designed by crystal framework.
+ * 
+ * @author Crystal Router Contributors https://github.com/shiny-crystals/router/graphs/contributors>.
+ * @package router.
+ */
 
+namespace Crystal\Router;
+
+/**
+ * Defines the structure for each route created.
+ */
+class Route implements RouteInterface
+{
+}
